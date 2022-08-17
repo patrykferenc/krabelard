db8016e7ca85e775e9ab3651fc1c9a2fbc6bf806
patrykferenc <patrykferenc@protonmail.com>
2022-08-17 22:47:48 +0200

    Initial project structure.

---
+++ server/src/main/java/dev/patrykferenc/krabelard/KrabelardServerApplication.java
@@ -0,0 +1,13 @@
+package dev.patrykferenc.krabelard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrabelardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrabelardServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/dev/patrykferenc/krabelard/KrabelardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patrykferenc.krabelard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrabelardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
