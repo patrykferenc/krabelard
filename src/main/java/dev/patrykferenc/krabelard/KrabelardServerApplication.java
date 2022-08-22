1107839f48f4315c0066f2dde026491908dff193
patrykferenc <patrykferenc@protonmail.com>
2022-08-22 23:21:50 +0200

    #6 - Add prettier and run reformat.

--- src/main/java/dev/patrykferenc/krabelard/KrabelardServerApplication.java
+++ src/main/java/dev/patrykferenc/krabelard/KrabelardServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KrabelardServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KrabelardServerApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(KrabelardServerApplication.class, args);
+  }
 }
