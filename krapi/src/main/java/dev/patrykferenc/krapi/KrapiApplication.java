72ea15e2d8fda61e609f3de2e5475e593deab3cc
patrykferenc <patrykferenc@protonmail.com>
2022-11-15 09:44:24 +0100

    KRB-80: Reinitialise krapi project

---
+++ krapi/src/main/java/dev/patrykferenc/krapi/KrapiApplication.java
@@ -0,0 +1,13 @@
+package dev.patrykferenc.krapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrapiApplication.class, args);
+	}
+
+}
--- krapi/src/test/java/dev/patrykferenc/krapi/KrapiApplicationTests.java
+++ krapi/src/test/java/dev/patrykferenc/krapi/KrapiApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.patrykferenc.krabelard;
+package dev.patrykferenc.krapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KrabelardServerApplicationTests {
+class KrapiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
