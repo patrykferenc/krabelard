1ebea4bb3420d1129a6efb17c260ac8f10aa22e8
patrykferenc <patrykferenc@protonmail.com>
2022-11-24 23:09:32 +0100

    Add vehicle positions project stub. #KRB-79

---
+++ vehicle-positions/src/main/java/pl/krabelard/vehiclepositions/VehiclePositionsApplication.java
@@ -0,0 +1,13 @@
+package pl.krabelard.vehiclepositions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclePositionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclePositionsApplication.class, args);
+    }
+
+}
---
+++ vehicle-positions/src/test/java/pl/krabelard/vehiclepositions/VehiclePositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krabelard.vehiclepositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclePositionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
