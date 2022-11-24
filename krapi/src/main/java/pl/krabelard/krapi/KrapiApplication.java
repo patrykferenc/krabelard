c0467e00f8a3012836993955a49032558d3dd1e8
patrykferenc <patrykferenc@protonmail.com>
2022-11-24 23:07:23 +0100

    Add krapi project stub. #KRB-79

---
+++ krapi/src/main/java/pl/krabelard/krapi/KrapiApplication.java
@@ -0,0 +1,13 @@
+package pl.krabelard.krapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrapiApplication.class, args);
+    }
+
+}
