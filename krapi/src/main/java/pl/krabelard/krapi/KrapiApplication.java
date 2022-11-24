110bed7a6502e8cec075adeb9e8632eb28b2b03e
patrykferenc <patrykferenc@protonmail.com>
2022-11-24 23:47:10 +0100

    Add project-wide formatting solution. #KRB-79

--- krapi/src/main/java/pl/krabelard/krapi/KrapiApplication.java
+++ krapi/src/main/java/pl/krabelard/krapi/KrapiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KrapiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KrapiApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(KrapiApplication.class, args);
+	}
 }
--- vehicle-positions/src/main/java/pl/krabelard/vehiclepositions/VehiclePositionsApplication.java
+++ vehicle-positions/src/main/java/pl/krabelard/vehiclepositions/VehiclePositionsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VehiclePositionsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VehiclePositionsApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclePositionsApplication.class, args);
+	}
 }
