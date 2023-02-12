4a83d7b1ddca7bd5c074f9a6c548339ef4822c2b
Maciej Niszczota <macni.macni@gmail.com>
2023-02-12 16:54:44 +0100

    Init commit

---
+++ overview/src/main/java/pl/krabelard/line/overview/OverviewApplication.java
@@ -0,0 +1,13 @@
+package pl.krabelard.line.overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverviewApplication.class, args);
+	}
+
+}
