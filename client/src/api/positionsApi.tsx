aaa6976225939ac9aea0d1e5041f0ff108ac5e34
Maciej Niszczota <macni.macni@gmail.com>
2023-04-22 20:51:53 +0200

    Add initial positions api

---
+++ client/src/api/positionsApi.tsx
@@ -0,0 +1,21 @@
+import { krapi } from "./krapi";
+
+const postionsClient = krapi("/vehicle-positions");
+
+export const positionsApi = {
+  getAllTramsPositions() {
+    return postionsClient.get(`/trams`);
+  },
+
+  getAllTramsPositionsOnLine(line: string) {
+    return postionsClient.get(`/trams/${line}`);
+  },
+
+  getAllBusesPositions() {
+    return postionsClient.get(`/buses`);
+  },
+
+  getAllBusesPositionsOnLine(line: string) {
+    return postionsClient.get(`/buses/${line}`);
+  },
+};
