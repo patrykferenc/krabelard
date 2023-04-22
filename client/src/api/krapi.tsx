b83df1c3c2ff696376992c3b6736e38b89624224
Maciej Niszczota <macni.macni@gmail.com>
2023-04-22 20:31:54 +0200

    Add krapi helloworld

---
+++ client/src/api/krapi.tsx
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+export const krapi = (url: string): AxiosInstance => {
+  let client: AxiosInstance = axios.create({
+    baseURL: url,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+
+  client.interceptors.response.use(
+    (response: AxiosResponse) => {
+      console.log("hello world");
+      return response;
+    },
+    function (error: any) {
+      console.log("An error occurred while calling krapi", error);
+      if (error.response.status === 404) {
+        return { status: error.response.status };
+      }
+      return Promise.reject(error.response);
+    }
+  );
+  return client;
+};
