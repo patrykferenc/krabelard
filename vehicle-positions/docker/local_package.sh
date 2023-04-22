1f4be0a46463038988133236eab682b582e35c55
patrykferenc <patrykferenc@protonmail.com>
2023-04-23 00:44:13 +0200

    #KRB-87 Add docker builds for local development.

---
+++ vehicle-positions/docker/local_package.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+cd "$(dirname "$0")"
+
+usage() {
+  echo "
+Usage: $0
+
+Builds zip file to simulate local pipeline.
+  "
+  exit 2
+}
+
+while getopts "h?" opt; do
+    case $opt in
+        h|*)
+            usage
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+main() {
+      echo "Preparing a jar file to simulate pipeline packaging..."
+      rm -rf b
+      mkdir b
+
+      (cd .. && ./gradlew clean bootJar)
+
+      cp -vR ../build/libs/vehicle-positions.jar b/
+
+      zip -r build.zip b/ -x "*.DS_Store"
+}
+
+main "$@"
