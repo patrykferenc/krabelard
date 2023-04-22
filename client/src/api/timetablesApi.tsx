b00751483d9fe1abf9c012c344001b222f444f9c
Maciej Niszczota <macni.macni@gmail.com>
2023-04-22 23:18:03 +0200

    Add scratch timetables api

---
+++ client/src/api/timetablesApi.tsx
@@ -0,0 +1,31 @@
+import { krapi } from "./krapi";
+
+const timetablesClient = krapi("/timetables");
+
+export const timetableApi = {
+  getAllLines() {
+    //TODO: add when GTFS feeds are ready
+    //get all lines from routes table
+    return null;
+  },
+
+  getDirectionsForLine(line: string) {
+    //TODO: add when GTFS feeds are ready
+    //get directions from routes table - substring
+    return null;
+  },
+
+  getAllStopsForLineAndDirection(line: string, direction: string) {
+    //TODO: add when GTFS feeds are ready
+    //get
+    return null;
+  },
+
+  getArrivalsForStopAndLine(
+    line: string,
+    stopName: string,
+    stopNumber: string
+  ) {
+    return timetablesClient.get(`/${stopName}/${stopNumber}/${line}`);
+  },
+};
