996fd1bfe9d079d05f608f68cc4cf64902d60433
Maciej Niszczota <macni.macni@gmail.com>
2023-06-03 23:53:33 +0200

    Add directions serving

--- lines/src/main/java/pl/krabelard/lines/controllers/LinesController.java
+++ lines/src/main/java/pl/krabelard/lines/controllers/LinesController.java
@@ -2,10 +2,8 @@ package pl.krabelard.lines.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.krabelard.lines.entities.DirectionsDTO;
 import pl.krabelard.lines.entities.LinesDTO;
 import pl.krabelard.lines.services.LineService;
 
@@ -18,6 +16,12 @@ public class LinesController {
     @ResponseStatus(HttpStatus.OK)
     public LinesDTO getAllLines(){
         return LineService.getAllLinesMocked();
+    }
+
+    @GetMapping("/{line}")
+    @ResponseStatus(HttpStatus.OK)
+    public DirectionsDTO getDirectionsForLine(@PathVariable("line") String line){
+        return LineService.getDirectionsMocked(line);
     }
 
 }
