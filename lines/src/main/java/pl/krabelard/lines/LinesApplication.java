fe466310191652c7218f5b1212f94b2362834fcb
Maciej Niszczota <macni.macni@gmail.com>
2023-06-03 22:10:39 +0200

    Initial commit

---
+++ lines/src/main/java/pl/krabelard/lines/LinesApplication.java
@@ -0,0 +1,13 @@
+package pl.krabelard.lines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinesApplication.class, args);
+	}
+
+}
