4d4c4289ac5ed0ec1dbaffe85940dba9e128ef99
Maciej Niszczota <macni.macni@gmail.com>
2023-06-03 22:10:39 +0200

    Initial commit

---
+++ lines/src/test/java/pl/krabelard/lines/LinesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krabelard.lines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
